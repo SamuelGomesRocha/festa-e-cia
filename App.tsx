83575b079c66adab07ca256a9eef82e19aaa8630
Samuel Gomes Rocha <samuhellgomes@outlook.com>
2021-05-25 11:42:10 -0300

    Quase nenhum progesso

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { } from 'react-native'
 import Routes from './src/routes'
+import SideBar from './src/routes'
+import DrawerContent from './src/routes'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Menu } from './src/pages/Menu'
+//import Drawer from './src/routes/drawer'
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   const x = useFonts({
@@ -14,6 +23,12 @@ export default function App() {
   });
 
   return (
-    <Routes />
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={Menu} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+
+    // <Routes />
   );
 }
